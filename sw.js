eb9eee6fa72bc330304fe4d7f47ce954dda02b04
vgovindam <102747047+vgovindam@users.noreply.github.com>
2026-09-07 16:06:02 -0500

    Refresh cache after Family Sync initialization fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sakhi-magic-learning-v9';
+const CACHE='sakhi-magic-learning-v10';
 const ASSETS=['./','./index.html','./styles.css','./interactions.css','./visuals.css','./kid-upgrade.css','./sakhi-shell.css','./app.js','./scheduler.js','./interaction-engine.js','./visuals.js','./kid-upgrade.js','./curriculum.js','./persistence.js','./adaptive-engine.js','./backend-bridge.js','./speech-service.js','./sakhi-shell.js','./supabase-config.js','./manifest.json','./unicorn-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
